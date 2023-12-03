d214e0f2663b863025ab84f7a227435c67ccba20
Tai Nguyen <tai.nguyen@wartsila.com>
2023-12-03 20:12:10 +0200

    Feat Project page frame

--- app/frontend/src/pages/Projects/Projects.tsx
+++ app/frontend/src/pages/Projects/Projects.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+import { getAllGithubRepos } from "../../api/api";
+
 const Projects = () => {
+    const [repos, setRepos] = useState([]);
+
+    const fetchAllGithubRepos = async () => {
+        try {
+          const githubRepos = await getAllGithubRepos();
+          setRepos(githubRepos);
+        } catch (error) {
+          console.error('Error fetching GitHub repos:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchAllGithubRepos()
+    },[]);
+
+
     return (
-        <div></div>
+        <div className="project-page">
+            <h2>My Projects</h2>
+        <div className="project-list">
+          {repos.map((repo) => (
+            <ProjectItem key={repo.id} repo={repo} />
+          ))}
+        </div>
+      </div>
     )
 }
 
